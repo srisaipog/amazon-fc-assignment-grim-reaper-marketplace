cfe8fe6e174cf30507f515f16392b95e10a60ad1
maryyw <30440359+maryyw@users.noreply.github.com>
2019-10-22 11:07:15 -0400

    Create project_functions.py

---
+++ project_functions.py
@@ -0,0 +1,30 @@
+from typing import Dict, List
+
+item = Shipment((input("Enter shipment item: "), input("Enter size of item in cm: "))
+
+
+class Shipment:
+    """
+    Attrs:
+        name (str): name of the item
+        size (int): in cm
+        category (str): category of item (food, )
+    """
+    def __init__(self, name: str, size: int, category: str):
+        self.name = name
+        self.size = size
+        self.category = category
+
+class Trolly:
+    """
+    Attrs:
+
+    """
+    def __init__(self, size: int):
+        self.size = size
+
+    def add_item_to_trolly():
+
+    def scan_item():
+        return f"This item is a(n) {Shipment}"
+
