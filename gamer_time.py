de7aeb9719b0d9de9fcbb8ade9ce334a23204fa7
MaxNaegel <maximilian.naegel21@ycdsbk12.ca>
2019-10-27 23:15:00 -0400

    gamrer time

--- gamer_time.py
+++ gamer_time.py
@@ -1,12 +1,14 @@
 from typing import List, Dict
 import json
 
+# maybe incvlude product tracking - im lazy :?
 class Trolly:
     all_trollies = []
-    def __init__(self):
+
+    def __init__(self, number: int):
         self.storage = []
-        self.status = False
         self.weight_capacity = 100
+        self.num = number
         all_trollies.append(self)
     
     def calculate_weight(self):
@@ -16,14 +18,14 @@ class Trolly:
         return total_weight
 
 
-
 class Product:
     incoming_products = []
+
     def __init__(self, name: str, weight: int, barcode: int, image: str):
         self.name = name
         self.barcode = barcode
         self.image = image
-        self.status = 'Incoming'
+        self.trolley_num = None
         Product.incoming_products.append(self)
     
     def __str__(self):
@@ -33,18 +35,22 @@ class Product:
         self.address = address
         self.weight = weight
         self.warning = warning
-        self.status = 'Packaged'
+
 
 class Shipment:
     def __init__(self, products: List[Product]):
         self.products = products
     
     def load_to_trolly(self):
-        available_trolly = True
         for product in Product.incoming_products:
             for trolly in Trolly.all_trollies:
                 if (trolly.calculate_weight() + product.weight) > 100:
                     continue
                 else:
-                    pass
+                    trolly.storage.append(product)
+                    product.trolly.num = trolly.num
+                    Product.incoming_products.remove(product)
+                    #end the function (indicates that a trolly was found)
+                    return False
+        return "The warehouse has run out of available trollies. Feel free to create a new one."
 
