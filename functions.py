305e8d76fc03f6550a5ae00e9808734facb1bf45
MaxNaegel <maximilian.naegel21@ycdsbk12.ca>
2019-10-29 20:51:10 -0400

    pickle library to store data in the json working now :D

--- functions.py
+++ functions.py
@@ -1,8 +1,16 @@
 from typing import List, Dict
-import json
+import pickle
 
-with open("data.json", "r") as f:
-    data = json.load(f)
+data = {"all_trollies": [], "incoming_products": []}
+
+def load():
+    with open("data.json", "rb") as f:
+        data = pickle.load(f)
+
+def set_up():
+    data = {'all_trollies': [], 'incoming_products': []}
+    with open("data.json", 'wb') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
 def reset():
     Trolly.all_trollies = []
@@ -12,10 +20,13 @@ def reset():
 # maybe incvlude product tracking - im lazy :?
 class Trolly:
     all_trollies = data["all_trollies"]
-    def __init__(self, num: int):
+    num_trolly = 0
+
+    def __init__(self):
         self.storage = []
         self.weight_capacity = 100
-        self.num = num
+        self.id = num_trolly
+        num_trolly += 1
         Trolly.all_trollies.append(self)
     
     def calculate_weight(self):
@@ -25,7 +36,7 @@ class Trolly:
         return total_weight
 
     def __str__(self):
-        return f"{self.num} = {self.storage}"
+        return f"{self.id} = {self.storage}"
 
 
 class Product:
@@ -33,7 +44,6 @@ class Product:
     def __init__(self, name: str, weight: int, barcode: int):
         self.name = name
         self.barcode = barcode
-        self.image = image
         self.trolley_num = 'None'
         Product.incoming_products.append(self)
     
@@ -61,7 +71,7 @@ class Shipment:
                     continue
                 else:
                     trolly.storage.append(product)
-                    product.trolly.num = trolly.num
+                    product.trolly.id = trolly.id
                     Product.incoming_products.remove(product)
         if len(Product.incoming_products) != original_size:
             return "There are " + str(len(Product.incoming_products)) + " remaining. Make more trollies to finish loading the products."
@@ -75,5 +85,11 @@ class Shipment:
 def save():
     data["all_trollies"] = Trolly.all_trollies
     data["incoming_products"] = Product.incoming_products
-    with open("data.json", "w") as f:
-        json.dump(data, f)
+    with open("data.json", "wb") as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+   main()
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from functions import *
 
 def main():
+    pizza = Product('pepperoni', 50, 1)
+    for product in Product.incoming_products:
+        print(product)
     
+    reset()
+    print(Product.incoming_products)
 
     
 if __name__ == '__main__':
